7530fbdc87f3decbd6680573ed71db5053197f16
Hannesde4 <hannes_de_baere@hotmail.com>
2019-03-05 19:28:14 +0100

    pfff

--- assets/js/Library/Math/Vector3.js
+++ assets/js/Library/Math/Vector3.js
@@ -1,80 +1,76 @@
 import Matrix3 from './Matrix3.js'
-/**
- * Three dimensional vector
- */
+
+/** Class representing a two-dimensional vector. */
 export default class Vector3 {
+    /**
+     * Create a vector.
+     * @param {Number} x - The horizontal vector component.
+     * @param {Number} y - The vertical vector component.
+     * @param {Number} z - The z-as vector component.
+     */
+    constructor(x, y, z) {
+        this.x = Number(x) || 0
+        this.y = Number(y) || 0
+        this.z = Number(z) || 0
+    }
 
-  /**
-   * Create a three dimensional vector
-   * @param {Number} xpos - position on X-axis
-   * @param {Number} ypos - position on Y-axis
-   * @param {Number} zpos - position on Z-axis
-   */
-  constructor(xpos, ypos, zpos) {
-    this.x = Number(xpos) || 0
-    this.y = Number(ypos) || 0
-    this.z = Number(zpos) || 0
-  }
+    /**
+     * Calculate the length of the vector.
+     * @return {Number} The length of the vector
+     */
+    norm() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2 + this.z ** 2)
+    }
 
-  /**
-   * Addition of 2 vectors
-   * @param {Vector3} v - Second vector
-   */
-  add(v) {
-    this.x += v.x
-    this.y += v.y
-    this.z += v.z
-  }
+    /**
+     * Addition of a vector to the current vector.
+     * @param {Vector3} v - The second vector.
+     */
+    add(v) {
+        this.x += v.x
+        this.y += v.y
+        this.z += v.z
+    }
 
-  /**
-   * Subtraction of 2 vectors
-   * @param {Vector3} v - Second vector
-   */
-  sub(v) {
-    this.x -= v.x
-    this.y -= v.y
-    this.z -= v.z
-  }
+    /**
+     * Subtraction of a vector from the current vector.
+     * @param {Vector3} v - The second vector.
+     */
+    sub(v) {
+        this.x -= v.x
+        this.y -= v.y
+        this.z -= v.z
+    }
 
-  /**
-   * Scalar multiplication
-   * @param {Number} a - Number to multiply by
-   */
-  scalar(a) {
-    this.x *= a
-    this.y *= a
-    this.z *= a
-  }
+    /**
+     * Scalar multiplication. Multiplies a vector by a scalar.
+     * @param {Number} a - The scalar value.
+     */
+    scalar(a) {
+        this.x *= a
+        this.y *= a
+        this.z *= a
+    }
 
-  /**
-   * Length of the vector
-   */
-  norm() {
-    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2) + Math.pow(this.z, 2))
-  }
+    /**
+     * Calculate the dot product of the current vector and another vector.
+     * @param {Vector3} v - The second vector.
+     * @return {Number} the dot product of the wzo
+     */
+    dot(v) {
+        return this.x * v.x + this.y * v.y + this.z * v.z
+    }
 
-  /**
-   * Dot product of two vectors
-   * @param {Vector2} v - Second vector
-   */
-  dot(v) {
-    return (this.x * v.x) + (this.y * v.y) + (this.z * v.z)
-  }
-
-  /**
-   * Rotate vector around origin
-   * @param {Number} α - Anticlockwise angle (degrees)
-   */
-  rot(α) {
-    const m = new Matrix3([
-      this.x, 0, 0,
-      this.y, 0, 0,
-      this.z, 0, 0
-    ])
-    m.rot(α)
-    console.log(m)
-    this.x = m.elements[0]
-    this.y = m.elements[3]
-    this.z = m.elements[6]
-  }
+    /**
+     * Rotate the vector around the origin.
+     * @param {Number} α - The anticlockwise angle in degrees.
+     */
+    rot(α) {
+        const m = new Matrix3([this.x, 0, this.y, 0, this.z, 0])
+        m.rot(α)
+        console.log(m)
+        this.x = m.elements[0]
+        this.y = m.elements[2]
+        this.z = m.elements[4]
+    }
 }
--- assets/js/Tests/Tests.js
+++ assets/js/Tests/Tests.js
@@ -1,10 +1,4 @@
 import TestMatrix2 from './Library/Math/TestMatrix2.js'
-import TestMatrix3 from './Library/Math/TestMatrix3.js'
-import TestMatrix4 from './Library/Math/TestMatrix4.js'
-
-import TestVector2 from './Library/Math/TestVector2.js'
-import TestVector3 from './Library/Math/TestVector3.js'
-import TestVector4 from './Library/Math/TestVector4.js'
 
 /** Class to run tests. */
 export default class Tests {
@@ -13,18 +7,6 @@ export default class Tests {
      */
     constructor() {
         console.log('Running Tests…')
-        new TestVector2()
-        console.log("================================================")
-        new TestVector3()
-        console.log("================================================")
-        new TestVector4()
-        console.log("================================================")
-
-
         new TestMatrix2()
-        console.log("================================================")
-        new TestMatrix3()
-        console.log("================================================")
-        new TestMatrix4()
     }
 }
